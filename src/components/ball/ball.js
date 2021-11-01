b23027deff1060e2c6d2d9e288157d40182958f5
TiredTorch <vitochek337@gmail.com>
2021-11-01 11:52:12 +0300

    Hotfix

--- src/components/ball/ball.js
+++ src/components/ball/ball.js
@@ -3,15 +3,19 @@ var arr = ["Yes","No", "Perhaps","Maybe","Sometimes"];
 
 export default class Ball extends Component{
     Say(){
-        document.getElementById('text').innerHTML = arr[
-            Math.floor(Math.random()*arr.length)
-        ];
+        if(document.getElementById('yours').value == ''){   
+            document.getElementById('text').innerHTML = "SAY YOUR QUESTION!";
+        } else{
+            document.getElementById('text').innerHTML = arr[
+                Math.floor(Math.random()*arr.length)
+            ];
+        }
         document.getElementById('yours').value = '';
     }
 
     render(){
         return(
-            <button className="ball" onClick={this.Say.bind(this)}>
+            <button className="ball" onClick={this.Say}>
             <span className="ball__text">
                 <span className="text" id="text">
                     Your prediction
